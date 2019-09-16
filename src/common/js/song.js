a9eced7d9d397eec1a4ac15353b767295e522788
lixinyang <623270307@qq.com>
2019-09-16 13:37:22 +0800

    修改歌曲不能播放问题

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -19,8 +19,7 @@ export function createSong (musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     img: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://ws.stream.qqmusic.qq.com/C100${musicData.songmid}.m4a?fromtag=0&guid=263427534`
-    // url: `http://aqqmusic.tc.qq.com/amobile.music.tc.qq.com/C400${musicData.songmid}.m4a?guid=263427534&vkey=&fromtag=66`
+    url: `http://111.202.85.150/amobile.music.tc.qq.com/C400${musicData.songmid}.m4a?guid=263427534&vkey=3738151029242D531256BDDF99A760E3482966D989879B90B883D685B5B399DB13D657704F907DD1557F3B6A9DFA8EF1C30E1FA8DD0A26D3&fromtag=66`
   })
 }
 function filterSonger (singer) {
